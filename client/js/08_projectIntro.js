81310332558c0f71cb7dc96df9b68b6cb57d8cd3
Chema Rivera <chema@Chemas-iMac.local>
2019-05-26 12:49:26 +0200

    Project intro updated

--- client/js/08_projectIntro.js
+++ client/js/08_projectIntro.js
@@ -1,16 +1,32 @@
 "use strict";
 
-$(function() {
-    const projectIntroText =    "Application for languages study focused on users adding the languages and content they wish" +
-                                " to study, in addition to the period in which they want that content to be distributed.<br>A" +
-                                " session is generated daily with the content that users should complete for that day, emphasizing" +
-                                " the mistakes made in previous sessions."
+document.body.classList.add("noScroll"); // Initial prevent scrolling while project intro is visible
 
-    $("<p>" + projectIntroText + "</p>").appendTo("#projectIntroText");
 
-    $(window).on("click touchstart", function(e) {
-        if ($("#projectIntroContainer").is(":visible") && ($(e.target).is("#projectIntroContainer") || $(e.target).is("#projectIntroExit"))) {
-            $("#projectIntroContainer").fadeOut("slow");
-        }
-    });
-});
+/* Project intro message */
+
+const projectIntroText =    "Application for languages study focused on users adding the languages and content they wish" +
+                            " to study, in addition to the period in which they want that content to be distributed.<br>A" +
+                            " session is generated daily with the content that users should complete for that day, emphasizing" +
+                            " the mistakes made in previous sessions.";
+
+document.getElementById("projectIntroText").innerHTML = projectIntroText;
+
+
+/* Text fade-in animation */
+
+document.getElementById("projectIntroContent").style.maxWidth = document.getElementById("projectIntroImg").offsetWidth + "px";
+setTimeout(() => {
+    document.getElementById("projectIntroContent").style.maxWidth = "900px";
+}, 500);
+
+
+/* Porject intro fade-out animation */
+
+function fadeOut() {
+    document.getElementById("projectIntroContainer").classList.add("fadeOut");
+    setTimeout(() => {
+        document.getElementById("projectIntroContainer").style.display = "none";
+        document.body.classList.remove("noScroll"); // Removes scrolling prevention
+    }, 500);
+}
