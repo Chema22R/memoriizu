32d3cad81aa38f128c506a14c2eaeb7cbc8f1b91
Chema <chema22r@gmail.com>
2019-10-07 13:02:57 +0200

    Project-intro updated

--- client/libs/projectIntro.js
+++ client/libs/projectIntro.js
@@ -1,14 +1,23 @@
 "use strict";
 
+const mobileType = navigator.userAgent.match(/Android|BlackBerry|iPhone|iPod|Opera Mini|IEMobile/i) && navigator.userAgent.match(/Mobile/i);
+const projectIntroTextInitialMinWidth = (window.innerWidth > 900) ? 600 : (window.innerWidth > 700) ? (600 - (900 - window.innerWidth)) : window.innerWidth;
+
 {
     document.body.classList.add("noScroll");
 
+    document.getElementById("projectIntroContainer").style.display = "flex";
     document.getElementById("projectIntroContent").style.maxWidth = document.getElementById("projectIntroImg").offsetWidth + "px";
+    mobileType ? document.getElementById("projectIntroContent").classList.add("mobile") : null;
     document.getElementById("projectIntroText").style.display = "unset";
+    document.getElementById("projectIntroText").style.minWidth = `${projectIntroTextInitialMinWidth}px`;
+    document.getElementById("projectIntroText").scrollTop = 0;
 
+    setTimeout(() => document.getElementById("projectIntroContent").style.maxWidth = "900px", mobileType ? 0 : 500);
     setTimeout(() => {
-        document.getElementById("projectIntroContent").style.maxWidth = "900px";
-    }, 500);
+        document.getElementById("projectIntroText").style.overflow = "auto";
+        document.getElementById("projectIntroText").style.minWidth  = "unset";
+    }, mobileType ? 0 : 2000);
 }
 
 
